47e34e660267927ae2471cdaa909fdb2b6a6559f
Yuntao Chen <chenyuntao08@gmail.com>
2019-06-07 08:42:16 +0800

    fix bug when #img > 1

--- tvm/decode_bbox.py
+++ tvm/decode_bbox.py
@@ -8,32 +8,48 @@ def _corner_to_center(F, boxes):
     return x, y, width, height
 
 def decode_bbox(F, anchors, deltas, im_infos, means, stds, class_agnostic):
-    ctr_x, ctr_y, width, height = _corner_to_center(F, anchors)
-    im_infos = F.split(im_infos, axis=-1, num_outputs=3, squeeze_axis=False)
+    """
+    anchors: (#img, #roi, #cls * 4)
+    deltas: (#img, #roi, #cls * 4)
+    im_infos: (#img, 3), [h, w, scale]
+    means: (4, ), [x, y, h, w]
+    stds: (4, ), [x, y, h, w]
+    class_agnostic: bool
+    """
+
+    # add roi axis, layout (img, roi, coord)
+    im_infos = F.expand_dims(im_infos, axis=1)
+
     if class_agnostic:
+        # TODO: class_agnostic should predict only 1 class
+        # class_agnostic predicts 2 classes
         deltas = F.slice_axis(deltas, axis=-1, begin=4, end=None)
     if not class_agnostic:
-        deltas = F.reshape(deltas, [0, 0, -4, -1, 4])
-        ctr_x = F.expand_dims(ctr_x, axis=-1)
-        ctr_y = F.expand_dims(ctr_y, axis=-1)
-        width = F.expand_dims(width, axis=-1)
-        height = F.expand_dims(height, axis=-1)
+        # add class axis, layout (img, roi, cls, coord)
+        deltas = F.reshape(deltas, [0, 0, -4, -1, 4])  # TODO: extend to multiple anchors
+        anchors = F.expand_dims(anchors, axis=-2)
+        im_infos = F.expand_dims(im_infos, axis=-2)
+
+    ax, ay, aw, ah = _corner_to_center(F, anchors)  # anchor
+    im_infos = F.split(im_infos, axis=-1, num_outputs=3)
     dx, dy, dw, dh = F.split(deltas, axis=-1, num_outputs=4)
 
+    # delta
     dx = dx * stds[0] + means[0]
     dy = dy * stds[1] + means[1]
     dw = dw * stds[2] + means[2]
     dh = dh * stds[3] + means[3]
 
-    pred_ctr_x = F.broadcast_add(F.broadcast_mul(dx, width), ctr_x)
-    pred_ctr_y = F.broadcast_add(F.broadcast_mul(dy, height), ctr_y)
-    pred_w = F.broadcast_mul(F.exp(dw), width)
-    pred_h = F.broadcast_mul(F.exp(dh), height)
+    # prediction
+    px = F.broadcast_add(F.broadcast_mul(dx, aw), ax)
+    py = F.broadcast_add(F.broadcast_mul(dy, ah), ay)
+    pw = F.broadcast_mul(F.exp(dw), aw)
+    ph = F.broadcast_mul(F.exp(dh), ah)
 
-    x1 = pred_ctr_x - 0.5 * (pred_w - 1.0)
-    y1 = pred_ctr_y - 0.5 * (pred_h - 1.0)
-    x2 = pred_ctr_x + 0.5 * (pred_w - 1.0)
-    y2 = pred_ctr_y + 0.5 * (pred_h - 1.0)
+    x1 = px - 0.5 * (pw - 1.0)
+    y1 = py - 0.5 * (ph - 1.0)
+    x2 = px + 0.5 * (pw - 1.0)
+    y2 = py + 0.5 * (ph - 1.0)
 
     x1 = F.maximum(F.broadcast_minimum(x1, im_infos[1] - 1.0), 0)
     y1 = F.maximum(F.broadcast_minimum(y1, im_infos[0] - 1.0), 0)
@@ -44,3 +60,35 @@ def decode_bbox(F, anchors, deltas, im_infos, means, stds, class_agnostic):
     if not class_agnostic:
         out = F.reshape(out, [0, 0, -3, -2])
     return out
+
+
+if __name__ == "__main__":
+    import mxnet as mx
+
+    anchors = mx.nd.array([10, 20, 55, 77, 10, 20, 50, 77]).reshape([2, 1, 4])
+    im_infos = mx.nd.array([76, 72, 1, 76, 76, 1]).reshape([2, 3])
+    deltas = mx.nd.array([0, 0, 0, 0.1, 0.3, 0.1, 0.2, 0.4, 0.5, 0.1, 0.2, 0.1, 0, 0, 0, 0, 0.3, 0.1, 0.2, 0.4, 0.5, 0.1, 0.2, 0.1]).reshape([2, 1, 12])
+    means = (0.04, 0.01, 0.03, 0.02)
+    stds = (0.5, 0.1, 0.1, 0.5)
+
+    o1 = mx.nd.contrib.DecodeBBox(
+        rois=anchors,
+        bbox_pred=deltas,
+        im_info=im_infos,
+        bbox_mean=means,
+        bbox_std=stds,
+        class_agnostic=False
+    )
+    o2 = decode_bbox(
+        mx.ndarray,
+        anchors,
+        deltas,
+        im_infos,
+        means,
+        stds,
+        False
+    )
+
+    print(o1)
+    print(o2)
+    print(o1 - o2)
