8f27ace04e07f1ec3e4955ce05d5d5e99b17e619
Yuntao Chen <chenyuntao08@gmail.com>
2019-08-18 09:56:42 +0800

    make mxnext a package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="mxnext",
+    version="0.0.1",
+    author="Yuntao Chen",
+    author_email="chenyuntao08@gmail.com",
+    description="a wrapper around original MXNet symbolic API",
+    url="https://github.com/RogerChern/mxnext",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
